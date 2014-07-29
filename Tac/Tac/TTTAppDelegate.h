8ec4c15b1b4d6388ae541ba582b5198eb7e53e0a
Merritt Tidwell <Merritt.tidwell@gmail.com>
2014-07-29 09:34:27 -0400

    create app

---
+++ Tac/Tac/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  Tac
+//
+//  Created by Merritt Tidwell on 7/29/14.
+//  Copyright (c) 2014 Merritt Tidwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
